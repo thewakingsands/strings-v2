77b92f89d75104d4a332b0b8073c4df9ef60c88a
zhyupe <zyp108421@gmail.com>
2025-12-21 10:45:45 +0800

    feat(ui): increase maxWidth of FixedWidthContainer to enhance layout flexibility

--- ui/src/components/FixedWidthContainer.tsx
+++ ui/src/components/FixedWidthContainer.tsx
@@ -2,7 +2,7 @@ import styled from '@emotion/styled'
 import type React from 'react'
 
 const MarginDiv = styled.div({
-  maxWidth: 1200,
+  maxWidth: 2160,
   margin: '0 auto',
   padding: '0 10px',
 })
