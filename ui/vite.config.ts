8a9e7849dcd38a464841c17db93391ad22c4c0e9
zhyupe <zyp108421@gmail.com>
2026-03-07 06:56:47 +0800

    chore: ignore api path in service worker

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,19 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
 import { resolve } from 'node:path'
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
-      jsxImportSource: "@emotion/react",
-  }), 
-    VitePWA({ registerType: 'autoUpdate' }),
+      jsxImportSource: '@emotion/react',
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        navigateFallbackDenylist: [
+          /^\/api\/.*/, // Ignores any path starting with /api/
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: {
-      "@": resolve(__dirname, "./src"),
+      '@': resolve(__dirname, './src'),
     },
   },
   server: {
